cdfbe69c7e4194e1d0e6792fbcaba85d0e61d18d
variant3a <sulfur.monoxide168@gmail.com>
2020-11-02 15:14:45 +0900

    add privacy policy

---
+++ database/migrations/2020_11_02_044847_create_friends_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->string('submit_user_id');
+            $table->string('accept_user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
