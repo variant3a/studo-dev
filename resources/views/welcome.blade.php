1d58bf2f5ad3411748d92322d9237648d185919d
variant3a <sulfur.monoxide168@gmail.com>
2021-02-02 12:32:26 +0900

    add home.blade backimage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,52 @@
 @section('description', 'トップページの説明が入ります')
 
 @section('content')
-    <div class="container" style="min-height:80vh;">
-        &nbsp;
-        <div class="row">
-            <div class="col s12 center">
-                <h5>プログラマーのための<br>
-                プログラミング勉強サポートアプリ</h5>
+    <div class="parallax-container" style="width: 100vw">
+        <div class="parallax"><img src="{{ asset('images/home_background_code.JPG') }}" style="height: 300%"></div>
+
+        <div class="row" style="margin: 5em 0 10em 0">
+            <div class="col s12 center white-text">
+                <h3 style="text-shadow: black 0 0 5px"><span class="inblock">プログラマーのための</span><br>
+                    <span class="inblock">プログラミング勉強</span><span class="inblock">サポート</span><span class="inblock">アプリ</span></h3>
             </div>
-        </div>
-        <div class="row">
-            <div class="center">
-                <h3>はじめる</h3>
+            <div class="col s12 center">
                 <a href="{{ route('login') }}" class="waves-effect waves-light btn-large">{{ __('Login') }}</a>
                 <a href="{{ route('register') }}" class="waves-effect waves-light btn-large">{{ __('Register') }}</a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row" style="margin: 5em 0">
+            <div class="col s12 center">
+                <h4>What is Studo?</h4>
+                <div class="">Studoでできること</div>
+                &nbsp;
+                <div class="row">
+                    <div class="col s12 m6 l3">
+                        <i class="material-icons large amber-text text-darken-1">timer</i>
+                        <div class="center">
+                            <h5>{{ __('Timer') }}</h5>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l3">
+                        <i class="material-icons large amber-text text-darken-1">library_books</i>
+                        <div class="center">
+                            <h5>{{ __('Notepad') }}</h5>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l3">
+                        <i class="material-icons large amber-text text-darken-1">rate_review</i>
+                        <div class="center">
+                            <h5>{{ __('Quiz') }}</h5>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l3">
+                        <i class="material-icons large amber-text text-darken-1">insert_chart</i>
+                        <div class="center">
+                            <h5><span class="inblock">勉強時間の</span><span class="inblock">記録</span></h5>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
